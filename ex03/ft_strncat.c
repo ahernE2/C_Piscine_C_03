d5b299c70f1467ad82a5cb38bf7f80adb12b0ebb
Alejandro Hernandez Hernandez <alejhern@cbr6s2.42barcelona.com>
2024-04-02 15:51:30 +0200

    cambio de nombre de variable

--- ex03/ft_strncat.c
+++ ex03/ft_strncat.c
@@ -6,11 +6,11 @@
 /*   By: alejhern <alejhern@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 11:15:27 by alejhern          #+#    #+#             */
-/*   Updated: 2024/04/02 11:18:14 by alejhern         ###   ########.fr       */
+/*   Updated: 2024/04/02 15:50:36 by alejhern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strncat(char *dest, char *src, unsigned int n)
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
 {
 	unsigned int	i;
 	unsigned int	j;
@@ -19,7 +19,7 @@ char	*ft_strncat(char *dest, char *src, unsigned int n)
 	j = 0;
 	while (dest[i])
 		i++;
-	while (src[j] && j < n)
+	while (src[j] && j < nb)
 	{
 		dest[i] = src[j];
 		i++;
