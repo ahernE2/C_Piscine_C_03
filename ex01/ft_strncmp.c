281a4c7142fb646c2952de39cec2e1be09c9ff4c
Alejandro Hernandez Hernandez <alejhern@cbr9s2.42barcelona.com>
2024-04-07 17:59:46 +0200

    Correccion Ejercicio 1

--- ex01/ft_strncmp.c
+++ ex01/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: alejhern <alejhern@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 10:50:50 by alejhern          #+#    #+#             */
-/*   Updated: 2024/04/02 10:55:38 by alejhern         ###   ########.fr       */
+/*   Updated: 2024/04/07 17:58:32 by alejhern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	unsigned int	i;
 
 	i = 0;
-	while (s1[i] && s1[i] == s2[i])
+	while (s1[i] && s1[i] == s2[i] && i < n - 1)
 		i++;
 	if (i == n)
 		return (0);
